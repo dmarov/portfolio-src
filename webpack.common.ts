01d8e5359f55a07007ba66d41c33120ee5b456be
Dmitry Marov <d.marov94@gmail.com>
2019-09-10 22:28:45 +0300

    update

--- webpack.common.ts
+++ webpack.common.ts
@@ -1,8 +1,21 @@
 import webpack from 'webpack';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 const config: webpack.Configuration = {
 
     entry: './src/index.ts',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Portfolio',
+            filename: 'index.html',
+        }),
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+    }
 };
 
 export default config;
