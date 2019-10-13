3e66f9e2fde500536fbd2fa4328b5c9a97349271
Dmitry Marov <d.marov94@gmail.com>
2019-10-13 15:48:37 +0300

    deploy 2019-10-13 15:48:37

--- src/components/home.vue
+++ src/components/home.vue
@@ -19,20 +19,30 @@ import logoGit from '../images/logo_git.png';
 @Component
 export default class Menu extends Vue {
 
-    logoPhp: any = logoPhp;
-    logoApache: any = logoApache;
-    logoNginx: any = logoNginx;
-    logoUnix: any = logoUnix;
-    logoLinux: any = logoLinux;
-    logoNode: any = logoNode;
-    logoMysql: any = logoMysql;
-    logoPostgresql: any = logoPostgresql;
-    logoWebpack: any = logoWebpack;
-    logoVuejs: any = logoVuejs;
-    logoGulp: any = logoGulp;
-    logoBabel: any = logoBabel;
-    logoGit: any = logoGit;
+    images: Array<any> = [
+        logoPhp,
+        logoApache,
+        logoNginx,
+        logoUnix,
+        logoLinux,
+        logoNode,
+        logoMysql,
+        logoPostgresql,
+        logoWebpack,
+        logoVuejs,
+        logoGulp,
+        logoBabel,
+        logoGit
+    ];
 
+    getStyle(index: number): object {
+
+        let res = {
+            'background-image': `url(${this.images[index]})`,
+        };
+
+        return res;
+    }
 }
 
 </script>
@@ -43,18 +53,7 @@ export default class Menu extends Vue {
         p I've been developing web sites for 2 years.
         p Also i developed services and administered servers.
         p This is my tech stack
-        p
-            img.tech-logo(:src='logoPhp')
-            img.tech-logo(:src='logoApache')
-            img.tech-logo(:src='logoNginx')
-            img.tech-logo(:src='logoUnix')
-            img.tech-logo(:src='logoLinux')
-            img.tech-logo(:src='logoNode')
-            img.tech-logo(:src='logoMysql')
-            img.tech-logo(:src='logoPostgresql')
-            img.tech-logo(:src='logoWebpack')
-            img.tech-logo(:src='logoVuejs')
-            img.tech-logo(:src='logoGulp')
-            img.tech-logo(:src='logoBabel')
-            img.tech-logo(:src='logoGit')
+        .tech-stack
+            .tech-logo-wrapper(v-for="(img, index) in images")
+                .tech-logo(:style='getStyle(index)')
 </template>
