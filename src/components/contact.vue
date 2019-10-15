334a6caaf109965c251274e563c7209fa1412cfc
Dmitry Marov <d.marov94@gmail.com>
2019-10-15 21:02:14 +0300

    deploy 2019-10-15 21:02:14

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -11,10 +11,10 @@ export default class Menu extends Vue {
 <template lang='pug'>
 
     .page
-        | You can get in touch with me via
+        h2 Contacts
         .contacts
             a.contacts__icon.contacts__mail(href="mailto:d.marov94@gmail.com")
-            a.contacts__icon.contacts__facebook(href="https://www.facebook.com/dvmarov")
-            a.contacts__icon.contacts__vk(href="https://vk.com/id223706950")
+            a.contacts__icon.contacts__facebook(href="https://www.facebook.com/dvmarov" target="_blank")
+            a.contacts__icon.contacts__vk(href="https://vk.com/id223706950" target="_blank")
 
 </template>
