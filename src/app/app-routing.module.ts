4fdcaed423256a7ea7209867df6b758d1f214e95
Dmitry Marov <Dmitry.Marov@vmedia.ca>
2020-04-03 23:31:33 +0300

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PageHomeComponent } from "./page-home/page-home.component";
+import { PageProjectsComponent } from "./page-projects/page-projects.component";
+import { PageAboutMeComponent } from "./page-about-me/page-about-me.component";
+import { PageContactsComponent } from "./page-contacts/page-contacts.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "", component: PageHomeComponent },
+    { path: "projects", component: PageProjectsComponent },
+    { path: "about-me", component: PageAboutMeComponent },
+    { path: "contacts", component: PageContactsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/page-about-me/page-about-me.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-about-me',
+  templateUrl: './page-about-me.component.html',
+  styleUrls: ['./page-about-me.component.scss']
+})
+export class PageAboutMeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
