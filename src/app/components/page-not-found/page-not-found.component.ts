06cd27e47c7ca1857852d6896f8ce381b486eaab
Dmitry Marov <d.marov94@gmail.com>
2020-11-14 10:18:10 +0300

    updated styles

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-page-not-found',
-  templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
+    selector: 'app-page-not-found',
+    templateUrl: './page-not-found.component.html',
+    styleUrls: ['./page-not-found.component.scss'],
+    host: {
+        class: 'app-layout__content-wrapper',
+    },
 })
 export class PageNotFoundComponent implements OnInit {
 
