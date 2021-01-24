f8c94fa2f96ab92d1aa42d4f4c309c64cebecdb3
Dmitry Marov <d.marov94@gmail.com>
2021-01-24 20:29:29 +0300

    fixed deploy script

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { SwiperModule, SWIPER_CONFIG, SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
-    direction: 'horizontal',
-    slidesPerView: 'auto'
-};
 
 import {
     MenuComponent,
@@ -29,15 +23,8 @@ import {
         PageAboutMeComponent,
     ],
     imports: [
-        SwiperModule,
         BrowserModule,
         AppRoutingModule,
-    ],
-    providers: [
-        {
-            provide: SWIPER_CONFIG,
-            useValue: DEFAULT_SWIPER_CONFIG
-        },
     ],
     bootstrap: [AppComponent]
 })
