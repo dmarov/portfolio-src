a80a210112fc9b8f76d2b234374d1a9d93eee428
Dmitry Marov <d.marov94@gmail.com>
2021-01-24 22:09:21 +0300

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
+ng build --prod
 mkdir deploy
 cd deploy
 git clone git@github.com:dmarov/dmarov.github.io.git ./
 rm -r ./**
 cp -r ../dist/portfolio/** ./
 git add -A
-git commit -m\"deploy $(date '+%Y-%m-%d %H:%M:%S')\"
+git commit -m"deploy $(date '+%Y-%m-%d %H:%M:%S')"
 git push
 cd ../
 rm -rdf ./deploy
