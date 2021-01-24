9109140e0dba6a640ec647405499f399f6a83e64
Dmitry Marov <d.marov94@gmail.com>
2021-01-24 19:22:39 +0300

    improved navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio';
+
+    constructor(
+      private router: Router,
+    ) {
+        router
+          .events
+          .subscribe(s => {
+              if (s instanceof NavigationEnd) {
+                  const tree = router.parseUrl(router.url);
+                  if (tree.fragment) {
+                      const element = document.querySelector("#" + tree.fragment);
+                      if (element) { element.scrollIntoView(true); }
+                  } else {
+                      window.scrollTo(0, 0);
+                  }
+              }
+        });
+    }
 }
