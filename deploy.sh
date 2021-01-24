f860d5a1ee15d23614c824bf204f1859b2c77a26
Dmitry Marov <d.marov94@gmail.com>
2021-01-24 21:52:33 +0300

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+mkdir deploy
+cd deploy
+git clone git@github.com:dmarov/dmarov.github.io.git
+rm -r ./**
+cp -r ../dist/portfolio/** ./
+git add -A
+git commit -m\"deploy $(date '+%Y-%m-%d %H:%M:%S')\"
+git push
