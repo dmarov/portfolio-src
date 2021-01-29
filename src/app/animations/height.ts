dee8d47b244a62261b4dee31c02ca85c108eea19
Dmitry Marov <d.marov94@gmail.com>
2021-01-29 21:49:48 +0300

    animation work in progress

--- src/app/animations/height.ts
+++ src/app/animations/height.ts
@@ -7,9 +7,23 @@ import {
   // ...
 } from '@angular/animations';
 
-export const height = trigger('grow', [
-  transition('void <=> *', []),
-  transition('* <=> *', [style({ height: '{{startHeight}}px', opacity: 0 }), animate('.3s ease')], {
-    params: { startHeight: 0 }
-  })
-]);
+// export const height = trigger('grow', [
+//   transition('void <=> *', []),
+//   transition('* <=> *', [style({ height: '{{startHeight}}px', opacity: 0 }), animate('.3s ease')], {
+//     params: { startHeight: 0 }
+//   })
+// ]);
+
+export const height = trigger('slideInOut', [
+      state('in', style({
+        overflow: 'hidden',
+        height: '*',
+      })),
+      state('out', style({
+        opacity: '0',
+        overflow: 'hidden',
+        height: '0px',
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,6 +33,7 @@ import { environment } from '../environments/environment';
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         AppRoutingModule,
         StoreBaseModule,
         StoreModule.forFeature(UiReducers.featureKey, UiReducers.reducer),
