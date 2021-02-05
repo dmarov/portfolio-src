fdc26da02f273e1882e10b6447c472cf768b6cc1
Dmitry Marov <d.marov94@gmail.com>
2021-02-06 01:09:51 +0300

    added intersection observer

--- src/app/utils/unfold-effect.ts
+++ src/app/utils/unfold-effect.ts
@@ -2,11 +2,21 @@ export class UnfoldEffect {
 
     static init() {
 
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.intersectionRatio <= 0) {
+                    return;
+                }
+
+                entry.target.classList.toggle('unfold--hidden', false);
+                entry.target.classList.toggle('unfold--left', false);
+                entry.target.classList.toggle('unfold--right', false);
+            });
+        });
+
         document.querySelectorAll('.unfold')
             .forEach(el => {
-                el.classList.toggle('unfold--hidden', false);
-                el.classList.toggle('unfold--left', false);
-                el.classList.toggle('unfold--right', false);
+                observer.observe(el);
             });
     }
 }
