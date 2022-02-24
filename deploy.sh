bd8937fe13472f3b72ebef766292a4ce409fab21
Dmitry Marov <d.marov94@gmail.com>
2022-02-24 19:55:31 +0300

    reverted side effects

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-ng build --prod
+yarn ng build --prod
 mkdir deploy
 cd deploy
 git clone git@github.com:dmarov/dmarov.github.io.git ./
-rm -r ./**
+rm -rdf ./**
 cp -r ../dist/portfolio/** ./
 git add -A
 git commit -m"deploy $(date '+%Y-%m-%d %H:%M:%S')"
