111354c558df4c71c3345e6ac9e444e4f021d3b2
Dmitry Marov <d.marov94@gmail.com>
2022-02-24 19:22:12 +0300

    added github header

--- src/app/components/page-main/page-main.component.ts
+++ src/app/components/page-main/page-main.component.ts
@@ -11,9 +11,15 @@ import { UnfoldEffect, InViewEffect } from '../../utils';
 })
 export class PageMainComponent implements OnInit, AfterViewInit {
 
+    private startDate = new Date("June 17, 2017 00:00:00")
+    public years: number = 0;
+
     constructor() { }
 
     ngOnInit(): void {
+        const years = (Date.now() - this.startDate.getTime()) / (1000 * 60 * 60 * 24 * 365);
+
+        this.years = Math.floor(years);
     }
 
     ngAfterViewInit() {
