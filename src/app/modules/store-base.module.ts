1f49c6a52f54185ac0470e1c43e260caf2337bd8
Dmitry Marov <d.marov94@gmail.com>
2022-03-02 21:13:36 +0300

    added router store

--- src/app/modules/store-base.module.ts
+++ src/app/modules/store-base.module.ts
@@ -2,6 +2,7 @@ import { StoreModule } from '@ngrx/store';
 import { NgModule } from '@angular/core';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
     declarations: [],
@@ -11,7 +12,7 @@ import { environment } from '../../environments/environment';
             maxAge: 25,
             logOnly: environment.production,
         }),
+        StoreRouterConnectingModule.forRoot(),
     ],
-
 })
 export class StoreBaseModule {}
