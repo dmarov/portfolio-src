5d3534596ab97de33e81ac997dd3ec8d6fd3cdab
Dmitry Marov <d.marov94@gmail.com>
2022-03-26 00:12:12 +0300

    fixed resources

--- scripts/copy-index.js
+++ scripts/copy-index.js
@@ -2,3 +2,4 @@ const fs = require('fs');
 
 fs.copyFileSync('dist/portfolio/en/index.html', 'dist/portfolio/index.html');
 fs.copyFileSync('dist/portfolio/en/404.html', 'dist/portfolio/404.html');
+fs.copyFileSync('dist/portfolio/en/favicon.ico', 'dist/portfolio/favicon.ico');
