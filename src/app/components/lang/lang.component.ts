940ae632630d366eceaa276fde319a86cf25f7fa
Dmitry Marov <d.marov94@gmail.com>
2022-04-16 15:15:29 +0300

    updated lang switcher

--- src/app/components/lang/lang.component.ts
+++ src/app/components/lang/lang.component.ts
@@ -1,18 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Actions, ofType } from '@ngrx/effects';
+import { ROUTER_NAVIGATED } from '@ngrx/router-store';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-lang',
     templateUrl: './lang.component.html',
 })
-export class LangComponent implements OnInit {
+export class LangComponent implements OnInit, OnDestroy {
 
     isEn = false;
     isRu = false;
 
-    constructor() { }
+    subscription = new Subscription();
+
+    path = '/';
+
+    constructor(
+        private readonly actions$: Actions,
+    ) { }
 
     ngOnInit(): void {
         this.isEn = window.location.pathname.startsWith("/en");
         this.isRu = window.location.pathname.startsWith("/ru");
+
+        this.subscription.add(
+            this.actions$.pipe(
+                ofType(ROUTER_NAVIGATED),
+            ).subscribe(() => {
+                this.path = window.location
+                    .pathname
+                    .replace(/^\/en/, '')
+                    .replace(/^\/ru/, '');
+            })
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
     }
 }
