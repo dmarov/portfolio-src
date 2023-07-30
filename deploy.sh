fe37bd23cbc7779892020f4172a26c4b11ccf5bd
Dmitry Marov <d.marov94@gmail.com>
2023-07-30 08:40:45 +0300

    improved daployer

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-npm run build:prod &&\
- mkdir deploy &&\
- cd deploy &&\
- git clone git@github.com:dmarov/dmarov.github.io.git ./ &&\
- rm -rdf ./** &&\
- cp -r ../dist/portfolio/** ./ &&\
- git add -A &&\
- git commit -m"deploy $(date '+%Y-%m-%d %H:%M:%S')" &&\
- git push &&\
- cd ../ &&\
- rm -rdf ./deploy
+npm run build:prod \
+&& mkdir deploy \
+&& cd deploy \
+&& git clone git@github.com:dmarov/dmarov.github.io.git ./ \
+&& rm -rdf ./** \
+&& cp -r ../dist/portfolio/** ./ \
+&& git add -A \
+&& git commit -m"deploy $(date '+%Y-%m-%d %H:%M:%S')" \
+&& git push \
+&& cd ../ \
+&& rm -rdf ./deploy
