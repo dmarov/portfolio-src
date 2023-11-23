4924f5a90e96b24d3791ff86d1696585e1b3a892
Dmitry Marov <d.marov94@gmail.com>
2023-11-23 21:32:25 +0300

    deploy script wip

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,36 @@
 const { spawn } = require("child_process");
 
+// $DEPLOY_DIR=../dmarov.github.io
+
+// npm run prettier \
+// && npm run lint \
+// && npm run lint:styles \
+// && npm run test \
+// && npm run build:prod \
+// && cd $DEPLOY_DIR
+// && rm -rdf ./** \
+// && cp -r ../protfolio-src/dist/portfolio/** ./ \
+// && git add -A \
+// && git commit -m"deploy $(date '+%Y-%m-%d %H:%M:%S')" \
+// && git push \
+// && cd ../protfolio-src
 class Deployer {
   async deploy() {
-    const cd = 'cd ../../dmarov.github.io';
-    const prettier = 'npm run prettier';
-    const lintScripts = 'npm run lint';
-    const lintStyles = 'npm run lint:styles';
-    const test = 'npm run test';
-    const build = 'npm run build:prod';
+    const prettier = "npm run prettier";
+    const lintScripts = "npm run lint";
+    const lintStyles = "npm run lint:styles";
+    const test = "npm run test";
+    const build = "npm run build:prod";
+    const cd = "cd ../../dmarov.github.io";
+    const pull = "git pull";
+    const rm = "rm -rdf ./**"
+    const copyDist = "cp -r ../protfolio-src/dist/portfolio/** ./";
+    const add = "git add -A";
+    const commit = "git commit -m \"deploy $(date '+%Y-%m-%d %H:%M:%S')\"";
+    const push = "git push";
+    const cdBack = "cd ../protfolio-src";
 
-    const cmd = `${cd} && ${prettier} && ${lintScripts} && ${lintStyles} && ${test} && ${build}`;
+    const cmd = `${prettier} && ${lintScripts} && ${lintStyles} && ${test} && ${build} && ${cd} && ${pull} && ${rm} && ${copyDist} && ${add} && ${commit} && ${push} && ${cdBack}`;
 
     this.execute(cmd, __dirname);
   }
