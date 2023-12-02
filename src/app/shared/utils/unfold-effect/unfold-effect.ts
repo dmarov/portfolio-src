ed5ca1677fab589c927d1bb4a50464a9a95f1837
Dmitry Marov <d.marov94@gmail.com>
2023-12-02 12:26:56 +0300

    updated util

--- src/app/shared/utils/unfold-effect/unfold-effect.ts
+++ src/app/shared/utils/unfold-effect/unfold-effect.ts
@@ -7,9 +7,6 @@ export class UnfoldEffect {
         }
 
         entry.target.classList.toggle("unfold_hidden", false);
-        entry.target.classList.toggle("unfold--left", false);
-        entry.target.classList.toggle("unfold--right", false);
-        entry.target.classList.toggle("unfold--down", false);
         observer.unobserve(entry.target);
       });
     });
