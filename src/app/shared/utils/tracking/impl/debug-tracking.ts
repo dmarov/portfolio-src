88792264eb6643345737abf09679133e963df947
Dmitry Marov <d.marov94@gmail.com>
2023-12-02 14:43:43 +0300

    fixed lint

--- src/app/shared/utils/tracking/impl/debug-tracking.ts
+++ src/app/shared/utils/tracking/impl/debug-tracking.ts
@@ -2,6 +2,7 @@ import { Tracking } from "../tracking";
 
 export class DebugTracking extends Tracking {
   public sendEvent(eventName: string, data: object): Promise<void> {
+    // eslint-disable-next-line no-console
     console.info(
       `Sending tracking event ${eventName} with data ${JSON.stringify(data)}`,
     );
