79367bd0bb3107db0aa075b5d531040d3b4b799b
Dmitry Marov <d.marov94@gmail.com>
2024-01-15 21:02:04 +0300

    fixed docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,5 +5,5 @@ npm install
 ng serve --host=0.0.0.0 --port=4200 --configuration=en --disable-host-check &
 ng serve --host=0.0.0.0 --port=4201 --configuration=ru --disable-host-check
 
-# Wait for any process to exit
+# Wait for every process to exit
 wait < <(jobs -p)
