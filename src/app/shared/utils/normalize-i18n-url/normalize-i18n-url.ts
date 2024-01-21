99264406614989b475ae842601b9c470bc22b59b
Dmitry Marov <d.marov94@gmail.com>
2024-01-21 19:24:58 +0300

    wip

--- src/app/shared/utils/normalize-i18n-url/normalize-i18n-url.ts
+++
@@ -1,9 +0,0 @@
-export const normalizeI18nUrl = (fullPath: string): string => {
-  const pos = fullPath.replace(/^\//, "").indexOf("/");
-
-  if (pos === -1 || pos === fullPath.length - 1) {
-    return "/";
-  }
-
-  return fullPath.substring(pos + 1, fullPath.length);
-};
