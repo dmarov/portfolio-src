49430d4f8bc9d2f2348d40382c03b5663ace7a84
Dmitry Marov <d.marov@gmail.com>
2024-02-10 21:54:05 +0300

    updated entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,12 @@
 
 npm install
 
+ENV_DIR=/app/src/environments
+
+if [ ! -f $ENV_DIR/environment.ts ]; then
+  cp $ENV_DIR/environment.example.ts $ENV_DIR/environment.ts
+fi
+
 ng serve --host=0.0.0.0 --port=4200 --configuration=en --disable-host-check &
 ng serve --host=0.0.0.0 --port=4201 --configuration=ru --disable-host-check &
 
