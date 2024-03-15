810d0949b00ae1f8499f17bdab5d1aa7a2bae359
Dmitry Marov <d.marov94@gmail.com>
2024-03-15 19:49:08 +0300

    fixed ci

--- src/app/shared/services/menu/menu-state.service.spec.ts
+++ src/app/shared/services/menu/menu-state.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from "@angular/core/testing";
 import { take, toArray } from "rxjs/operators";
-import { Features } from "@/app/models/features/features.class";
 import { MenuStateInterface } from "./menu-state.interface";
 import { MenuStateService } from "./menu-state.service";
 import { MENU_STATE } from "../../const/injection-tokens.const";
@@ -8,20 +7,12 @@ import { MENU_STATE } from "../../const/injection-tokens.const";
 describe("MenuStateService", () => {
   let service: MenuStateInterface;
 
-  const mockFeatures: Features = {
-    SHOW_WHAT_I_STAND_FOR: true,
-    RESPONSIVE_MENU_ENABLED: false,
-  };
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         {
           provide: MENU_STATE,
           useClass: MenuStateService,
-        },
-        {
-          provide: Features,
-          useValue: mockFeatures,
         },
       ],
     });
