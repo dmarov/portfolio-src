2f296a5b285e4f91eca9ef6febd11eb45dff4965
Dmitry Marov <d.marov94@gmail.com>
2024-03-21 18:51:28 +0300

    refactored

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
-import { RouterTestingModule } from "@angular/router/testing";
+import { RouterModule } from "@angular/router";
 import { AppComponent } from "@/app.component";
 import {
   LANGUAGE_SWITCH,
@@ -27,7 +27,7 @@ describe("AppComponent", () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [AppComponent, RouterTestingModule, NoopAnimationsModule],
+      imports: [AppComponent, NoopAnimationsModule, RouterModule.forRoot([])],
       providers: [
         {
           provide: TRACKING,
